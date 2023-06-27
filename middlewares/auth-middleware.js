00a778c7bc12bce2a7c1fb01a19e3bdee96953e4
leeseungjun <cjstkrak@gmail.com>
2023-06-28 01:37:04 +0900

    오류 1 userInfos.js : 모델에 User-Infos 를 UserInfos 로 수정 해결
    오류2
    : userId 가 데이터를 삭제해도
    이어서 늘어나는 현상
    미해결
    
    오류3
    users.route.js
    : 사용자 조회 API
    include 에
    UserInfos를 참조하는 코드가 있는데
    아마 model에서 참조키 연결을 안해서 생기는 오류(같음)
    미해결
    
    오류4
    auth-middleware.js
    :
    미들웨어에서 오류가 생기면
    const [tokenType, token] = authorization.split(" ");
    부분이 try 밖에있어서 서버가 다운되는현상
    try 안으로 옮김
    해결
    
    ==> 인증과 연결해 postsCRUD 완성

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,26 +1,31 @@
-const jwt = require("jsonwebtoken");   //사용자 인증미들웨어
+const jwt = require("jsonwebtoken"); //사용자 인증미들웨어
 const { Users } = require("../models");
 
 module.exports = async (req, res, next) => {
+  try {
     const { authorization } = req.cookies;
     const [tokenType, token] = authorization.split(" ");
     if (tokenType !== "Bearer" || !token) {
-      return res.status(401).json({ message: "토큰 타입이 일치하지 않습니다." });
+      return res
+        .status(401)
+        .json({ message: "토큰 타입이 일치하지 않습니다." });
     }
-  try {
+
     const decodedToken = jwt.verify(token, "customized_secret_key");
     const userId = decodedToken.userId;
 
     const user = await Users.findOne({ where: { userId } });
     if (!user) {
       //res.clearCookie("authorization");
-      return res.status(401).json({ message: "토큰 사용자가 존재하지 않습니다." });
+      return res
+        .status(401)
+        .json({ message: "토큰 사용자가 존재하지 않습니다." });
     }
     res.locals.user = user;
     next();
   } catch (error) {
     return res.status(401).json({
-      message: "비정상적인 요청입니다."
+      message: "비정상적인 요청입니다.",
     });
   }
-}
+};
