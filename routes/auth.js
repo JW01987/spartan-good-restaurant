2f83f86aa5f74d9e22597b9855554cbc96651a40
Jean <etnpfj@gmail.com>
2023-06-28 08:58:37 +0900

    refactor: users.route.js 의 코드 일부분을 auth.js로 옮김

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,44 @@
-// const express = require("express");
-// const router = express.Router();
-// const jwt = require("jsonwebtoken");//확인받기
-// const User = require("../schemas/user");//확인받기
-// //코드는 여기
-// //로그인 API
-// router.post("/auth", async (req, res) => {
-//     const { email, password } = req.body;
-//   //이메일이 일치하는 유저를찾는다
-//     const user = await User.findOne({ email });
-  
-//     // NOTE: 인증 메세지는 자세히 설명하지 않는것을 원칙으로 한다.
-//     if (!user || user.password !== password) {
-//       res.status(400).json({
-//         errorMessage: "로그인에 실패하였습니다.",
-//       });
-//       return;
-//     }
-//   //jwt를 생성
-//     const token = jwt.sign({userId: user.userId},"customized-secret-key",);
-  
-//       res.cookie("Authorization", `Bearer ${token}`); // JWT를 Cookie로 할당합니다!
-//     res.status(200).json({token}); // JWT를 Body로 할당합니다!
-//   });
-  
-// module.exports = router;
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken"); //확인받기
+const { Users } = require("../models"); //확인받기
+
+// 로그인//
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await Users.findOne({
+    where: { email },
+  });
+  //해당사용자 존재,비번존재
+  if (!user) {
+    return res
+      .status(401)
+      .json({ errorMessage: "존재하지 않는 이메일입니다." });
+  } else if (user.password !== password) {
+    return res
+      .status(401)
+      .json({ errorMessage: "비밀번호가 일치하지 않습니다." });
+  }
+  //jwt를 생성
+  const token = jwt.sign(
+    {
+      userId: user.userId,
+    },
+    "customized_secret_key"
+  );
+  //쿠키를발급
+  res.cookie("authorization", `Bearer ${token}`);
+  //response 할당
+  return res.status(200).json({ message: "로그인 성공" });
+});
+
+//로그아웃 API
+router.post("/logout", (req, res) => {
+  //클라에서 해당경로로 요청을 보내면 서버는 쿠키의 authorization토큰을삭제
+  // 클라이언트에서 토큰 삭제
+  res.clearCookie("authorization"); //메서드를 사용하여 클라 쿠키 삭제
+
+  return res.status(200).json({ message: "로그아웃되었습니다." }); //응답값
+});
+
+module.exports = router;
--- routes/comments.route.js
+++ routes/comments.route.js
@@ -1,12 +1,12 @@
 const { Comments } = require("../models");
 const { Posts } = require("../models");
 const express = require("express");
-//const authMiddleware = require("../middlewares/auth-middleware"); //나중에 수정
+const authMiddleware = require("../middlewares/auth-middleware");
 const router = express.Router();
 const { Op } = require("sequelize");
 
 //댓글 생성
-router.post("/posts/:postId/comments", async (req, res) => {
+router.post("/posts/:postId/comments", authMiddleware, async (req, res) => {
   const { postId } = req.params;
   const { content } = req.body;
   const { userId } = res.locals.user;
@@ -54,74 +54,84 @@ router.get("/posts/:postId/comments", async (req, res) => {
 });
 
 //댓글 수정
-router.put("/posts/:postId/comments/:commentId", async (req, res) => {
-  const { postId, commentId } = req.params;
-  const { content } = req.body;
-  const { userId } = res.locals.user;
-  if (!content.length) {
-    return res
-      .status(412)
-      .json({ errorMessage: "데이터 형식이 올바르지 않습니다." });
-  }
-  const postIdCheck = await Posts.findOne({ where: { postId } });
-  if (!postIdCheck) {
-    return res
-      .status(404)
-      .json({ errorMessage: "게시글이 존재하지 않습니다." });
-  }
-  try {
-    const comment = await Comments.findOne({ where: { commentId } });
-    if (comment.userId !== userId) {
+router.put(
+  "/posts/:postId/comments/:commentId",
+  authMiddleware,
+  async (req, res) => {
+    const { postId, commentId } = req.params;
+    const { content } = req.body;
+    const { userId } = res.locals.user;
+    if (!content.length) {
       return res
-        .status(403)
-        .json({ errorMessage: "댓글의 수정 권한이 존재하지 않습니다." });
+        .status(412)
+        .json({ errorMessage: "데이터 형식이 올바르지 않습니다." });
     }
-  } catch {
-    return res.status(404).json({ errorMessage: "댓글이 존재하지 않습니다." });
-  }
-  try {
-    await Comments.update(
-      { content }, // title과 content 컬럼을 수정합니다.
-      {
-        where: {
-          [Op.and]: [{ commentId }, { userId }, { postId }],
-        },
+    const postIdCheck = await Posts.findOne({ where: { postId } });
+    if (!postIdCheck) {
+      return res
+        .status(404)
+        .json({ errorMessage: "게시글이 존재하지 않습니다." });
+    }
+    try {
+      const comment = await Comments.findOne({ where: { commentId } });
+      if (comment.userId !== userId) {
+        return res
+          .status(403)
+          .json({ errorMessage: "댓글의 수정 권한이 존재하지 않습니다." });
       }
-    );
-    res.status(200).json({ message: "댓글이 수정되었습니다." });
-  } catch {
-    res
-      .status(400)
-      .json({ errorMessage: "댓글 수정이 정상적으로 처리되지 않았습니다." });
+    } catch {
+      return res
+        .status(404)
+        .json({ errorMessage: "댓글이 존재하지 않습니다." });
+    }
+    try {
+      await Comments.update(
+        { content }, // title과 content 컬럼을 수정합니다.
+        {
+          where: {
+            [Op.and]: [{ commentId }, { userId }, { postId }],
+          },
+        }
+      );
+      res.status(200).json({ message: "댓글이 수정되었습니다." });
+    } catch {
+      res
+        .status(400)
+        .json({ errorMessage: "댓글 수정이 정상적으로 처리되지 않았습니다." });
+    }
+    res.status(400).json({ errorMessage: "댓글 수정에 실패하였습니다." });
   }
-  res.status(400).json({ errorMessage: "댓글 수정에 실패하였습니다." });
-});
+);
 
 //댓글 삭제
-router.delete("/posts/:postId/comments/:commentId", async (req, res) => {
-  const { postId, commentId } = req.params;
-  const { userId } = res.locals.user;
-  try {
-    const comment = await Comments.findOne({ where: { commentId, postId } });
-    if (comment.userId !== userId) {
+router.delete(
+  "/posts/:postId/comments/:commentId",
+  authMiddleware,
+  async (req, res) => {
+    const { postId, commentId } = req.params;
+    const { userId } = res.locals.user;
+    try {
+      const comment = await Comments.findOne({ where: { commentId, postId } });
+      if (comment.userId !== userId) {
+        return res
+          .status(403)
+          .json({ errorMessage: "댓글의 삭제 권한이 존재하지 않습니다." });
+      }
+    } catch {
       return res
-        .status(403)
-        .json({ errorMessage: "댓글의 삭제 권한이 존재하지 않습니다." });
+        .status(404)
+        .json({ errorMessage: "댓글 삭제가 정상적으로 처리되지 않았습니다." });
     }
-  } catch {
-    return res
-      .status(404)
-      .json({ errorMessage: "댓글 삭제가 정상적으로 처리되지 않았습니다." });
+    try {
+      await Comments.destroy({
+        where: {
+          [Op.and]: [{ commentId }],
+        },
+      });
+      res.status(200).json({ message: "댓글이 삭제되었습니다." });
+    } catch {
+      res.status(400).json({ errorMessage: " 댓글 삭제에 실패하였습니다." });
+    }
   }
-  try {
-    await Comments.destroy({
-      where: {
-        [Op.and]: [{ commentId }],
-      },
-    });
-    res.status(200).json({ message: "댓글이 삭제되었습니다." });
-  } catch {
-    res.status(400).json({ errorMessage: " 댓글 삭제에 실패하였습니다." });
-  }
-});
+);
 module.exports = router;
