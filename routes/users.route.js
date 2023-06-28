eaf9578e30c554c97f59036feb3a9c3c59496ef0
Jean <etnpfj@gmail.com>
2023-06-28 16:03:20 +0900

    fix: 비밀번호 암호화 코드 수정

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const crypto = require("crypto");
 const router = express.Router();
-const { Users, UserInfos } = require('../models'); 
+const { Users, UserInfos } = require("../models");
+const CryptoJS = require("crypto-js");
+const PRIVATE_KEY = "secret key";
 
 // 회원가입
-router.post('/users', async (req, res) => {
+router.post("/users", async (req, res) => {
   const { email, password, nickname, age, gender, introduce } = req.body;
   const isExistUser = await Users.findOne({
     where: {
@@ -13,37 +14,28 @@ router.post('/users', async (req, res) => {
   });
   // email과 동일한 유저가 실제로 존재할 때 에러 발생
   if (isExistUser) {
-    return res.status(409).json({ message: '이미 존재하는 이메일입니다.' });
+    return res.status(409).json({ message: "이미 존재하는 이메일입니다." });
   }
 
-  // // 사용자 테이블에 데이터삽입
-  // const user = await Users.create({ email, password });
-  // // 사용자 정보 테이블에 데이터 삽입
-  // await UserInfos.create({
-  //   UserId: user.userId, //현재 사용자 정보가 19번째 줄에서 생성된 사용자의 userId를 할당합니다
-  //   nickname,
-  //   age,
-  //   gender,
-  //   introduce,
-  // });    위에 코드는 필요없을꺼같고 
-           //비밀번호 암호화하는코드넣으면서 에러떠서 일단 주석처리했습니다
-
- // 비밀번호 암호화//
-const hashedPassword = crypto // hash 함수를 일반 텍스트 암호로 갱신
-  createHash("sha256")//createHash메서드를 사용하여 암호화 해시함수만듬 //해싱 알고리즘을 sha로 지정 
-  update(password)
-  digest("hex");// 해시된 암호를 16진수 형식으로 생성
-
-const user = await Users.create({ email, password: hashedPassword });
-await UserInfos.create({
-  UserId: user.userId,
-  nickname,
-  age,
-  gender,
-  introduce,
-});
-
-  return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
+  // 비밀번호 암호화//
+  const encrypted = CryptoJS.AES.encrypt(
+    JSON.stringify(password),
+    PRIVATE_KEY
+  ).toString();
+  try {
+    const user = await Users.create({ email, password: encrypted });
+    await UserInfos.create({
+      userId: user.id,
+      nickname,
+      age,
+      gender,
+      introduce,
+    });
+    return res.status(201).json({ message: "회원가입이 완료되었습니다." });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ errorMessage: "회원가입에 실패했습니다." });
+  }
 });
 
 module.exports = router;
