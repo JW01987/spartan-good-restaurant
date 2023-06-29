8ec44e64b333d28cc20df3db74c27e15f4c4bd7d
Siho <siho8808@icloud.com>
2023-06-29 20:01:58 +0900

    이벤트 누락 추가

--- assets/detail.js
+++ assets/detail.js
@@ -1,99 +1,99 @@
 //detailhtml 과 연결되어 있는 js파일
 
-import { renderPostDetails } from "./modules/GET-postDetails.js"; // 렌더링하는 모듈
-import { renderComments, saveComments } from "./modules/comments.js";
+import { renderPostDetails } from './modules/GET-postDetails.js'; // 렌더링하는 모듈
+import { renderComments, saveComments } from './modules/comments.js';
 
 renderPostDetails();
 renderComments();
 
-const signupLink = document.getElementById("signup-link");
-const registerOverlay = document.getElementById("register-overlay");
-const loginButton = document.getElementById("login-button");
-const loginOverlay = document.getElementById("login-overlay");
-const loginLink = document.getElementById("login-link");
-const closeLoginIcon = document.getElementById("login-close-icon");
-const closeRegisterIcon = document.getElementById("register-close-icon");
-const registerForm = document.getElementById("register-form");
-const loginForm = document.getElementById("login-form");
-const newPostButton = document.getElementById("new-post-button");
-const nicknameButton = document.getElementById("nickname-button");
-const userMenuButton = document.getElementById("user-menu-button");
-const userMenuUI = document.getElementById("user-menu");
-const homeButton = document.getElementById("home-button");
-const insertCommentButton = document.getElementById("comments-btn");
+const signupLink = document.getElementById('signup-link');
+const registerOverlay = document.getElementById('register-overlay');
+const loginButton = document.getElementById('login-button');
+const loginOverlay = document.getElementById('login-overlay');
+const loginLink = document.getElementById('login-link');
+const closeLoginIcon = document.getElementById('login-close-icon');
+const closeRegisterIcon = document.getElementById('register-close-icon');
+const registerForm = document.getElementById('register-form');
+const loginForm = document.getElementById('login-form');
+const newPostButton = document.getElementById('new-post-button');
+const nicknameButton = document.getElementById('nickname-button');
+const userMenuButton = document.getElementById('user-menu-button');
+const userMenuUI = document.getElementById('user-menu');
+const homeButton = document.getElementById('home-button');
+const insertCommentButton = document.getElementById('comments-btn');
 
-insertCommentButton.addEventListener("click", saveComments); // 댓글저장
+insertCommentButton.addEventListener('click', saveComments); // 댓글저장
 
 // 홈으로가기
-homeButton.addEventListener("click", () => {
-  location.href = "./index.html";
+homeButton.addEventListener('click', () => {
+  location.href = './index.html';
 });
 // 로그인 버튼 클릭 시 login-overlay 표시
-loginButton.addEventListener("click", () => {
-  loginOverlay.style.display = "flex";
+loginButton.addEventListener('click', () => {
+  loginOverlay.style.display = 'flex';
 });
 
 // 회원가입 버튼 클릭 시 register-overlay 표시
-signupLink.addEventListener("click", () => {
-  loginOverlay.style.display = "none";
-  registerOverlay.style.display = "flex";
+signupLink.addEventListener('click', () => {
+  loginOverlay.style.display = 'none';
+  registerOverlay.style.display = 'flex';
 });
 
 // register-overlay의 로그인 버튼 클릭 시 login-overlay 표시
-loginLink.addEventListener("click", () => {
-  registerOverlay.style.display = "none";
-  loginOverlay.style.display = "flex";
+loginLink.addEventListener('click', () => {
+  registerOverlay.style.display = 'none';
+  loginOverlay.style.display = 'flex';
 });
 
 // 로그인 창 닫기
-closeLoginIcon.addEventListener("click", () => {
-  loginOverlay.style.display = "none";
+closeLoginIcon.addEventListener('click', () => {
+  loginOverlay.style.display = 'none';
 });
 
 // 회원가입 창 닫기
-closeRegisterIcon.addEventListener("click", () => {
-  registerOverlay.style.display = "none";
+closeRegisterIcon.addEventListener('click', () => {
+  registerOverlay.style.display = 'none';
 });
 
 function showLoggedInUI(nickname) {
-  loginButton.style.display = "none";
-  userMenuButton.style.display = "block";
-  newPostButton.style.display = "block";
-  nicknameButton.style.display = "block";
+  loginButton.style.display = 'none';
+  userMenuButton.style.display = 'block';
+  newPostButton.style.display = 'block';
+  nicknameButton.style.display = 'block';
   nicknameButton.textContent = nickname;
 }
 
 function showLoggedOutUI() {
-  loginButton.style.display = "block";
-  userMenuButton.style.display = "none";
-  newPostButton.style.display = "none";
-  nicknameButton.style.display = "none";
-  userMenuUI.style.display = "none";
+  loginButton.style.display = 'block';
+  userMenuButton.style.display = 'none';
+  newPostButton.style.display = 'none';
+  nicknameButton.style.display = 'none';
+  userMenuUI.style.display = 'none';
 }
 
 function getCookie(name) {
-  const cookies = document.cookie.split(";");
+  const cookies = document.cookie.split(';');
 
   for (const cookie of cookies) {
-    const [cookieName, cookieValue] = cookie.trim().split("=");
+    const [cookieName, cookieValue] = cookie.trim().split('=');
     if (cookieName === name) {
       return decodeURIComponent(cookieValue);
     }
   }
 
-  return "";
+  return '';
 }
 
-window.addEventListener("DOMContentLoaded", async () => {
+window.addEventListener('DOMContentLoaded', async () => {
   {
-    const authorization = getCookie("authorization");
+    const authorization = getCookie('authorization');
 
     if (authorization) {
       try {
-        const profileResponse = await fetch("/api/profile", {
-          method: "GET",
+        const profileResponse = await fetch('/api/profile', {
+          method: 'GET',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
             Authorization: authorization,
           },
         });
@@ -106,7 +106,7 @@ window.addEventListener("DOMContentLoaded", async () => {
           showLoggedOutUI();
         }
       } catch (error) {
-        console.error("Error:", error);
+        console.error('Error:', error);
       }
     } else {
       // 인증 토큰이 없는 경우 로그인 버튼으로 유지
@@ -115,20 +115,20 @@ window.addEventListener("DOMContentLoaded", async () => {
   }
 });
 
-registerForm.addEventListener("submit", async (event) => {
+registerForm.addEventListener('submit', async (event) => {
   event.preventDefault(); // 폼 제출 기본 동작 방지
 
-  const email = document.getElementById("register-inputEmail").value;
-  const password = document.getElementById("register-inputPassword").value;
-  const nickname = document.getElementById("register-inputname").value;
-  const age = document.getElementById("register-inputAge").value;
-  const introduce = document.getElementById("register-inputIntroduce").value;
+  const email = document.getElementById('register-inputEmail').value;
+  const password = document.getElementById('register-inputPassword').value;
+  const nickname = document.getElementById('register-inputname').value;
+  const age = document.getElementById('register-inputAge').value;
+  const introduce = document.getElementById('register-inputIntroduce').value;
 
   try {
-    const response = await fetch("/api/users", {
-      method: "POST",
+    const response = await fetch('/api/users', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         email,
@@ -140,28 +140,28 @@ registerForm.addEventListener("submit", async (event) => {
     });
 
     if (response.ok) {
-      alert("회원가입이 완료되었습니다."); // 성공 메시지 표시
-      registerOverlay.style.display = "none"; // 회원가입 창 닫기
+      alert('회원가입이 완료되었습니다.'); // 성공 메시지 표시
+      registerOverlay.style.display = 'none'; // 회원가입 창 닫기
     } else {
       const data = await response.json();
       alert(data.message); // 실패 메시지 표시
     }
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
   }
 });
 
-loginForm.addEventListener("submit", async (event) => {
+loginForm.addEventListener('submit', async (event) => {
   event.preventDefault(); // 폼 제출 기본 동작 방지
 
-  const email = document.getElementById("inputEmail").value;
-  const password = document.getElementById("inputPassword").value;
+  const email = document.getElementById('inputEmail').value;
+  const password = document.getElementById('inputPassword').value;
 
   try {
-    const response = await fetch("/api/login", {
-      method: "POST",
+    const response = await fetch('/api/login', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         email,
@@ -170,14 +170,14 @@ loginForm.addEventListener("submit", async (event) => {
     });
 
     if (response.ok) {
-      alert("로그인이 완료되었습니다."); // 성공 메시지 표시
-      loginOverlay.style.display = "none"; // 로그인 창 닫기
+      alert('로그인이 완료되었습니다.'); // 성공 메시지 표시
+      loginOverlay.style.display = 'none'; // 로그인 창 닫기
 
       // 사용자 프로필 정보를 가져오기 위해 API 요청을 수행합니다.
-      const profileResponse = await fetch("/api/profile", {
-        method: "GET",
+      const profileResponse = await fetch('/api/profile', {
+        method: 'GET',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       });
 
@@ -187,45 +187,49 @@ loginForm.addEventListener("submit", async (event) => {
         const { nickname } = profileData.data.UserInfo;
         showLoggedInUI(nickname); // 사용자 닉네임으로 UI 업데이트
       } else {
-        alert("프로필을 불러올 수 없습니다."); // 실패 메시지 표시
+        alert('프로필을 불러올 수 없습니다.'); // 실패 메시지 표시
       }
     } else {
       const data = await response.json();
       alert(data.message); // 실패 메시지 표시
     }
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
   }
 });
 
-userMenuButton.addEventListener("click", () => {
+userMenuButton.addEventListener('click', () => {
   userMenuUI.style.display =
-    userMenuUI.style.display === "none" ? "block" : "none";
+    userMenuUI.style.display === 'none' ? 'block' : 'none';
 });
 
-const logoutButton = document.getElementById("logout-button");
+const logoutButton = document.getElementById('logout-button');
 
-logoutButton.addEventListener("click", async () => {
+logoutButton.addEventListener('click', async () => {
   {
     // 로그아웃 API 요청을 수행합니다.
-    fetch("/api/logout", {
-      method: "POST",
+    fetch('/api/logout', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     })
       .then((response) => {
         if (response.ok) {
           // 로그아웃이 성공하면 UI 업데이트
           showLoggedOutUI();
-          alert("로그아웃되었습니다.");
+          alert('로그아웃되었습니다.');
         } else {
-          alert("로그아웃을 할 수 없습니다.");
+          alert('로그아웃을 할 수 없습니다.');
         }
       })
       .catch((error) => {
-        console.error("Error:", error);
-        alert("로그아웃을 할 수 없습니다.");
+        console.error('Error:', error);
+        alert('로그아웃을 할 수 없습니다.');
       });
   }
+});
+
+newPostButton.addEventListener('click', async () => {
+  window.location.replace('./Post.html');
 });
--- assets/post.js
+++ assets/post.js
@@ -149,3 +149,7 @@ postForm.addEventListener('submit', async (data) => {
     alert('게시글 생성에 실패했습니다.');
   }
 });
+
+newPostButton.addEventListener('click', async () => {
+  window.location.replace('./Post.html');
+});
