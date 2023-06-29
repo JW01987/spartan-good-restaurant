9b1a31fa4cdfa2edd1dfd861c3f15e399cee4c0f
leeseungjun <cjstkrak@gmail.com>
2023-06-30 05:09:38 +0900

    댓글수정삭제

--- assets/modules/comments.js
+++ assets/modules/comments.js
@@ -20,7 +20,7 @@ async function getComments() {
 }
 
 async function renderComments() {
-  let { comment: comments } = await getComments(); // 객체구조분해할당 방식으로 변수저장을 해줘야한다. getMovie()로 가져온 데이터는 배열이기 때문에 객체로 변환
+  let { comment: comments } = await getComments();
   let html = "";
 
   comments.map((comment) => {
@@ -66,4 +66,174 @@ async function saveComments() {
   // });
 }
 
-export { renderComments, saveComments };
+//  댓글 수정
+async function modifyComments(e) {
+  if (e.target.classList.contains("modify-Btn")) {
+    // console.log(e.target.parentElement.id);
+    // console.log("Target : ", e.target);
+    // console.log("Target : ", e.target.parentElement);
+    // console.log(document.getElementById(e.target.parentElement.id));
+    let { comment: comments } = await getComments();
+    let currentComment = comments.filter(
+      (comment) => comment.id == e.target.parentElement.id
+    );
+    //
+    const modifybox = document.getElementById(e.target.parentElement.id);
+    modifybox.innerHTML = `
+    <div>닉네임 : ${currentComment[0].User.UserInfo.nickname}</div>
+  <div><a>수정할 내용</a><input class='modifyContent' value='${currentComment[0].content}'></div>
+  <button class="modify-Btn" id='verify'>확인</button>
+  <button class="cancel-Btn" id='cancel'>취소</button>`;
+
+    // 확인버튼 눌렀을때
+    const verifyBtn = document.getElementById("verify");
+    verifyBtn.addEventListener("click", () => {
+      let reviewContent = document.querySelector(".modifyContent").value;
+      if (!reviewContent) {
+        alert("내용입력해주세요");
+        return;
+      }
+      fetch(`/api/posts/${postId}/comments/${currentComment[0].id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: reviewContent,
+        }),
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          if (data.message) {
+            alert(data.message);
+            return location.reload();
+          } else if (data.errorMessage) {
+            alert(data.errorMessage);
+            return location.reload();
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          alert("수정 오류입니다.");
+        });
+    });
+    // 취소 버튼 눌렀을 때
+    const cancelBtn = document.getElementById("cancel");
+    cancelBtn.addEventListener("click", () => {
+      alert("수정취소");
+      window.location.reload();
+    });
+  } else if (e.target.classList.contains("delete-Btn")) {
+    {
+      // console.log(e.target.parentElement.id);
+      // console.log("Target : ", e.target);
+      // console.log("Target : ", e.target.parentElement);
+      // console.log(document.getElementById(e.target.parentElement.id));
+      let { comment: comments } = await getComments();
+      let currentComment = comments.filter(
+        (comment) => comment.id == e.target.parentElement.id
+      );
+      //
+      const deletebox = document.getElementById(e.target.parentElement.id);
+      deletebox.innerHTML = `
+      <div>닉네임 : ${currentComment[0].User.UserInfo.nickname}</div>
+  <div><a>content : ${currentComment[0].content}</a></div>
+  <button id='deleteVerify'>정말 삭제하시겠습니까?</button>
+  <button class="cancel-Btn" id='cancel'>취소</button>`;
+
+      // 확인버튼 눌렀을때
+      const deleteVerifyBtn = document.getElementById("deleteVerify");
+      deleteVerifyBtn.addEventListener("click", () => {
+        fetch(`/api/posts/${postId}/comments/${currentComment[0].id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            if (data.message) {
+              alert(data.message);
+              return location.reload();
+            } else if (data.errorMessage) {
+              alert(data.errorMessage);
+              return location.reload();
+            }
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+            alert("삭제 오류입니다.");
+          });
+      });
+      // 취소 버튼 눌렀을 때
+      const cancelBtn = document.getElementById("cancel");
+      cancelBtn.addEventListener("click", () => {
+        alert("삭제취소");
+        window.location.reload();
+      });
+    }
+  }
+}
+
+// 댓글삭제
+// async function deleteComments(e) {
+//   if (e.target.classList.contains("delete-Btn")) {
+//     // console.log(e.target.parentElement.id);
+//     // console.log("Target : ", e.target);
+//     // console.log("Target : ", e.target.parentElement);
+//     // console.log(document.getElementById(e.target.parentElement.id));
+//     let { comment: comments } = await getComments();
+//     let currentComment = comments.filter(
+//       (comment) => comment.id == e.target.parentElement.id
+//     );
+//     //
+//     const deletebox = document.getElementById(e.target.parentElement.id);
+//     deletebox.innerHTML = `
+//     <div>이름 : ${currentComment[0].id}</div>
+//   <div><a>content : '${currentComment[0].content}'</a></div>
+//   <button class="modify-Btn" id='deleteVerify'>정말 삭제하시겠습니까?</button>
+//   <button class="cancel-Btn" id='cancel'>취소</button>`;
+
+//     // 확인버튼 눌렀을때
+//     const deleteVerifyBtn = document.getElementById("deleteVerify");
+//     deleteVerifyBtn.addEventListener("click", () => {
+//       fetch(`/api/posts/${postId}/comments/${currentComment[0].id}`, {
+//         method: "DELETE",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           content: reviewContent,
+//         }),
+//       })
+//         .then((response) => {
+//           return response.json();
+//         })
+//         .then((data) => {
+//           if (data.message) {
+//             alert(data.message);
+//             return location.reload();
+//           } else if (data.errorMessage) {
+//             alert(data.errorMessage);
+//             return location.reload();
+//           }
+//         })
+//         .catch((error) => {
+//           console.error("Error:", error);
+//           alert("삭제 오류입니다.");
+//         });
+//     });
+//     // 취소 버튼 눌렀을 때
+//     const cancelBtn = document.getElementById("cancel");
+//     cancelBtn.addEventListener("click", () => {
+//       alert("삭제취소");
+//       window.location.reload();
+//     });
+//   }
+// }
+
+export { renderComments, saveComments, modifyComments };
