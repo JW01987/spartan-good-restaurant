ceb6db7b53db4270f03fb95e300bc62e80156876
Jean <etnpfj@gmail.com>
2023-06-30 14:58:31 +0900

    fix: 이미지 경로 문제 수정

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const {
   commentRouter,
   postRouter,
   usersRouter,
   userInfoRouter,
   authRouter,
-} = require('./routes');
+} = require("./routes");
 
+const path = require("path");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('assets')); //정적파일 사용하기 위해, assets의 html, css, js, 이미지 등
-
-app.use('/api', [
+// app.use(express.static('assets')); //정적파일 사용하기 위해, assets의 html, css, js, 이미지 등
+const publicPath = path.join(__dirname, "assets");
+app.use(express.static(publicPath));
+app.use("/api", [
   postRouter,
   commentRouter,
   usersRouter,
@@ -24,5 +26,5 @@ app.use('/api', [
 ]);
 
 app.listen(port, () => {
-  console.log(port, '포트로 서버가 열렸어요!');
+  console.log(port, "포트로 서버가 열렸어요!");
 });
--- assets/modules/GET-posts.js
+++ assets/modules/GET-posts.js
@@ -1,14 +1,14 @@
 async function getPosts() {
   let posts;
   const options = {
-    method: 'GET',
+    method: "GET",
   };
   try {
     posts = await fetch(`/api/posts`, options);
     // console.log(movies);
     // 패치로 가져온 데이터를 movies에 할당
   } catch (error) {
-    alert('오류'); // 오류시 오류알림
+    alert("오류"); // 오류시 오류알림
   }
   return posts.json(); // 받은 데이터를 json 형식으로 반환
 }
@@ -18,11 +18,11 @@ async function renderPosts() {
   let { data: posts } = await getPosts(); // 객체구조분해할당 방식으로 변수저장을 해줘야한다. getMovie()로 가져온 데이터는 배열이기 때문에 객체로 변환
   console.log(posts);
 
-  let html = '';
+  let html = "";
 
   posts.map((post) => {
     console.log(post.image);
-    let imagePath = post.image.replace('assets/', '');
+    let imagePath = post.image.replace("assets/", "");
     let image = `http://localhost:3000/${imagePath}`;
 
     let htmlSegment = `<div class="item-2">
@@ -44,7 +44,7 @@ async function renderPosts() {
     html += htmlSegment;
   });
 
-  let container = document.querySelector('.band');
+  let container = document.querySelector(".band");
   container.innerHTML = html;
 }
 
